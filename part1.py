4e85dcceac79eb4438e69fd83efdfdc514a7e630
Nicholas Stafford <ngstafford1@gmail.com>
2025-03-21 04:16:10 -0400

    made both run

--- part1.py
+++ part1.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     model_name = "part1"
     paths = constants.get_data_folder(model_name)
     file_list = setup_part1_data.generate_data_from_zip(paths)
-    # cnn.run(model_name, training_folder_path=paths.training_folder_path, num_classes=3)
+    cnn.run(model_name, training_folder_path=paths.training_folder_path, num_classes=3)
