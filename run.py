3e750035a6c9473763e00a1dc7d318ce221ac516
Nicholas Stafford <ngstafford1@gmail.com>
2025-03-20 20:23:39 -0400

    tested out refactor. working fine

--- run.py
+++ run.py
@@ -1,8 +1,5 @@
-from typing import Any
-from project1 import unzip, split_data, cnn, dataset
+from project1 import unzip, cnn
 
 if __name__ == "__main__":
     file_list = unzip.create_all_data(100)
-    # datasets = split_data.create_datasets(file_list)
-    # train_ds, val_ds = dataset.create_dataset()
     cnn.run()
